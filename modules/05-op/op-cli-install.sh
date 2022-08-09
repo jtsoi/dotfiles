b2e5b826f8cf5810fcbccbed472e17c0207e9ff9
jtsoi <jt@nulltales.com>
2022-08-09 18:15:29 +0200

    Update to OP 2.6

--- modules/05-op/op-cli-install.sh
+++ modules/05-op/op-cli-install.sh
@@ -2,11 +2,11 @@
 
 . ../../lib/dot.sh
 
-OP_CLI_VERSION='1.12.4'
+OP_CLI_VERSION='2.6.0'
 OP_CLI_PATH="/usr/local/bin/op"
 
 dot_curl_download \
-  "https://cache.agilebits.com/dist/1P/op/pkg/v$OP_CLI_VERSION/op_linux_amd64_v$OP_CLI_VERSION.zip" \
+  "https://cache.agilebits.com/dist/1P/op2/pkg/v$OP_CLI_VERSION/op_linux_amd64_v$OP_CLI_VERSION.zip" \
   "/tmp/op_cli.zip"
 
 unzip -o "/tmp/op_cli.zip" -d "/tmp/"
--- modules/06-dot-env/get-env-file.sh
+++ modules/06-dot-env/get-env-file.sh
@@ -1,11 +1,13 @@
 #!/bin/sh -Cue
 
+# Make sure you are logged in to "my.1password.eu" before running this script.
+
 . ../../lib/dot.sh
 
-OP_ITEM_NAME='dotfile-env'
+OP_ITEM_NAME='dotfiles-env'
 
-eval "$(op signin my)"
+eval "$(op signin --account my)"
 
-op get item dotfiles-env --fields notesPlain | tee "$DOT_ENV_FILE"
+op item get $OP_ITEM_NAME --fields notesPlain --format json | jq '.value' -r | tee "$DOT_ENV_FILE"
 
 echo ">>> '.env' file updated"
